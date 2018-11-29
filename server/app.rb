9201fe43073f23447a769248c5af51cdc7a2d233
Ajay <ajaymungur@hotmail.com>
2018-11-29 17:43:10 +0000

    added to app.rb

--- server/app.rb
+++ server/app.rb
@@ -8,7 +8,10 @@ class ThermostatApp < Sinatra::Base
   enable :sessions
   enable :cross_origin
 
+
   before do
+    session[:city] = "London, GB" if session[:city].nil?
+    session[:temperature] = "19" if session[:temperature].nil?
     response.headers['Access-Control-Allow-Origin'] = '*'
   end
 
@@ -17,8 +20,17 @@ class ThermostatApp < Sinatra::Base
   end
 
   get '/json' do
+    city = session[:city]
+    temperature = session[:temperature]
     content_type :json
-    { :city => "London, GB", :temperature => "22" }.to_json
+    { :city => city, :temperature => temperature }.to_json
+  end
+
+  get '/jsonset' do
+    session[:temperature] = params[:temperature] unless params[:temperature].nil?
+    p session[:temperature]
+    p params[:temperature]
+    p session[:city] = params[:city] unless params[:city].nil?
   end
 
 end
