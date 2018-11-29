1f5303f1f4154ed3e8b33a884f88ea52724482f5
Stuart Adair <stu@silverka.co.uk>
2018-11-29 16:52:24 +0000

    Reads defaults from our own API

--- server/app.rb
+++ server/app.rb
@@ -1,12 +1,24 @@
 require 'sinatra/base'
+require 'sinatra/cross_origin'
+require 'json'
 
 class ThermostatApp < Sinatra::Base
   run! if app_file == $0
 
   enable :sessions
+  enable :cross_origin
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
 
   get '/' do
     erb :index
   end
-  
+
+  get '/json' do
+    content_type :json
+    { :city => "London, GB", :temperature => "22" }.to_json
+  end
+
 end
